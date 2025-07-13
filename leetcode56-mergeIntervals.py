f40057b8b4ed7ddc1ed8d1d760af75abe25edd70
khoanguyn1411 <khoaah1411@gmail.com>
2025-07-13 22:47:01 +0700

    Add solution

--- leetcode56-mergeIntervals.py
+++ leetcode56-mergeIntervals.py
@@ -1,27 +1,49 @@
 from typing import List
 
 
+# class Solution:
+#     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+#         n = len(intervals)
+
+#         # Step 1: Sort intervals by the start time
+#         intervals.sort(key=lambda x: x[0])
+
+#         result = []
+#         for i in range(n):
+#             if i < n - 1 and (intervals[i][0] <= intervals[i + 1][0] <= intervals[i][1]
+#                               or intervals[i][0] <= intervals[i + 1][1] <= intervals[i][1]
+#                               or intervals[i + 1][0] <= intervals[i][1] <= intervals[i + 1][1]
+#                               or intervals[i + 1][0] <= intervals[i][1] <= intervals[i + 1][1]
+#                               ):
+#                 new_array = intervals[i] + intervals[i + 1]
+#                 new_array.sort()
+#                 new_entity = [new_array[0], new_array[3]]
+#                 intervals[i + 1] = new_entity
+#             else:
+#                 result.append(intervals[i])
+#         return result
+
+
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        n = len(intervals)
+        if not intervals:
+            return []
 
         # Step 1: Sort intervals by the start time
         intervals.sort(key=lambda x: x[0])
 
-        result = []
-        for i in range(n):
-            if i < n - 1 and (intervals[i][0] <= intervals[i + 1][0] <= intervals[i][1]
-                              or intervals[i][0] <= intervals[i + 1][1] <= intervals[i][1]
-                              or intervals[i + 1][0] <= intervals[i][1] <= intervals[i + 1][1]
-                              or intervals[i + 1][0] <= intervals[i][1] <= intervals[i + 1][1]
-                              ):
-                new_array = intervals[i] + intervals[i + 1]
-                new_array.sort()
-                new_entity = [new_array[0], new_array[3]]
-                intervals[i + 1] = new_entity
+        merged = [intervals[0]]
+
+        for current in intervals[1:]:
+            last = merged[-1]
+
+            # Step 2: If overlapping, merge them
+            if current[0] <= last[1]:
+                last[1] = max(last[1], current[1])  # extend the end
             else:
-                result.append(intervals[i])
-        return result
+                merged.append(current)  # no overlap, new interval
+
+        return merged
 
 
 solution = Solution()
