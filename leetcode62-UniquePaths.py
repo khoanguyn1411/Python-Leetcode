44302250e09f05e68f413f8458820bf04b05bbff
khoanguyn1411 <khoaah1411@gmail.com>
2025-08-03 15:53:36 +0700

    Add another solution

--- leetcode62-UniquePaths.py
+++ leetcode62-UniquePaths.py
@@ -20,5 +20,15 @@ class Solution:
 
         return recurse(0, 0)
 
+# class Solution:
+#     def uniquePaths(self, m: int, n: int) -> int:
+#         dp = [[1] * n for _ in range(m)]
+
+#         for i in range(1, m):
+#             for j in range(1, n):
+#                 dp[i][j] = dp[i-1][j] + dp[i][j-1]
+
+#         return dp[m-1][n-1]
+
 
 print(Solution().uniquePaths(3, 7))
