482fd0c32bf876806f93dbc0f7857bfc9c1ee1ce
khoanguyn1411 <khoaah1411@gmail.com>
2025-08-10 21:37:54 +0700

    Add leetcode 72

--- leetcode72-EditDistance.py
+++ leetcode72-EditDistance.py
@@ -1,9 +1,8 @@
-from functools import lru_cache
-
 
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        @lru_cache(None)
+        memo = {}
+
         def dp(i, j):
             # If word1 is empty, insert all chars of word2
             if i == len(word1):
@@ -12,14 +11,20 @@ class Solution:
             if j == len(word2):
                 return len(word1) - i
 
+            if (i, j) in memo:
+                return memo[(i, j)]
+
             if word1[i] == word2[j]:
                 return dp(i + 1, j + 1)  # no cost
+
             else:
-                return 1 + min(
+                min_steps = 1 + min(
                     dp(i + 1, j),     # delete
                     dp(i, j + 1),     # insert
                     dp(i + 1, j + 1)  # replace
                 )
+                memo[(i, j)] = min_steps
+                return min_steps
 
         return dp(0, 0)
 
