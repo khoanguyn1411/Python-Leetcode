891bb95e2df8c3218e0ed112ea7563d352767527
khoanguyn1411 <khoaah1411@gmail.com>
2025-08-17 23:06:07 +0700

    Add leetcode 80

--- leetcode80-RemoveDuplicatesFromSortedArrayII.py
+++ leetcode80-RemoveDuplicatesFromSortedArrayII.py
@@ -3,33 +3,19 @@ from typing import List
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        left = 0
-        i = 0
-        n = len(nums)
-        count = 0
-        while i < n:
-            k = 0
-            if nums[i] == nums[left]:
-                i += 1
-                count += 1
-                if count < 2:
-                    left += 1
-            else:
-                if count > 2:
-                    k = left + 1
-                    count = 1
-                    while k < i:
-                        nums[k] = nums[i]
-                        k += 1
-                        count += 1
-                    k = 0
-                    left += 2
-                    i += 1
-                else:
-                    count = 0
-                    left = i
-                    i += 1
-        return left
+        if len(nums) <= 2:
+            return len(nums)
+
+        # slow pointer (where to place next valid element)
+        slow = 2
+
+        # fast pointer (scan array)
+        for fast in range(2, len(nums)):
+            if nums[fast] != nums[slow - 2]:
+                nums[slow] = nums[fast]
+                slow += 1
+
+        return slow
 
 
 print(Solution().removeDuplicates([0, 0, 1, 1, 1, 1, 2, 3, 3]))
